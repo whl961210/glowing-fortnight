01b8cece12d18b7b5843459ddbd363c93ed38dac
hanlun <whl961210@gmail.com>
2023-11-19 01:22:08 -0800

    completed the simple analysis feature and it could load more comments

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -4,7 +4,8 @@ const ResultsTable = ({ results, sentimentPercentages }) => {
     if (!results || results.length === 0) {
         return <p>No results to display.</p>;
     }
-
+    console.log("Results:", results);
+    console.log("Sentiment Percentages:", sentimentPercentages);
     // Extract keys from the first result object
     const keys = Object.keys(results[0]);
 
@@ -30,13 +31,21 @@ const ResultsTable = ({ results, sentimentPercentages }) => {
                     </tbody>
                 </table>
             </div>
-            {/* Display sentiment percentages */}
-            {sentimentPercentages && Object.keys(sentimentPercentages).length > 0 && (
-                <div>
-                    <p>Positive: {sentimentPercentages.Positive}%</p>
-                    <p>Negative: {sentimentPercentages.Negative}%</p>
-                </div>
-            )}
+            {/* Display sentiment percentages if provided */}
+            {sentimentPercentages && (
+    <div>
+        <h2>Sentiment Results:</h2>
+        <ul>
+            {Object.entries(sentimentPercentages).map(([key, value]) => (
+                <li key={key}>
+                    {value.Negative && <span> Negative: {Number(value.Negative).toFixed(2)}% </span>}
+                    {value.Positive && <span> Positive: {Number(value.Positive).toFixed(2)}% </span>}
+                </li>
+            ))}
+        </ul>
+    </div>
+)}
+
 
         </div>
     );
