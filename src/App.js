ac5aa73bd9c27e712ddd41aedd995036f2c5f9df
hanlun <whl961210@gmail.com>
2023-11-21 13:44:06 -0800

    modified layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css'; // Default styling
 import TextInput from './components/TextInput';
 import FileUploader from './components/FileUploader';
 import ResultsTable from './components/ResultsTable';
 import YouTubeCommentAnalyzer from './components/YouTubeCommentAnalyzer';
 import apiService from './services/apiService';
+import './App.css'; // Your custom styles
 
 function App() {
     const [results, setResults] = useState([]);
     const [sentimentPercentages, setSentimentPercentages] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState('');
-
+    const hasSentimentData = sentimentPercentages && Object.keys(sentimentPercentages).length > 0;
     const handleTextSubmit = async (text) => {
         setIsLoading(true);
         setError('');
@@ -56,28 +59,53 @@ function App() {
     <div className="app-container">
         <h1 className="app-title">Sentiment Analysis</h1>
         {error && <div className="error-message">{error}</div>}
-
         {isLoading ? (
             <div className="loading-indicator">Loading...</div>
         ) : (
             <>
-                <div className="component-container">
-                    <TextInput onTextSubmit={handleTextSubmit} />
-                </div>
-                <div className="component-container">
-                    <FileUploader onFileUpload={handleFileUpload} />
-                </div>
-                <div className="component-container">
-                    <YouTubeCommentAnalyzer onAnalyzeYouTubeComments={handleAnalyzeYouTubeComments} />
-                </div>
-                <div className="component-container">
-                    <ResultsTable results={results} sentimentPercentages={sentimentPercentages} />
+                <Tabs>
+                    <TabList>
+                        <Tab>Text Analysis</Tab>
+                        <Tab>File Upload</Tab>
+                        <Tab>YouTube Comments</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                        <TextInput onTextSubmit={handleTextSubmit} />
+                    </TabPanel>
+                    <TabPanel>
+                        <FileUploader onFileUpload={handleFileUpload} />
+                    </TabPanel>
+                    <TabPanel>
+                        <YouTubeCommentAnalyzer onAnalyzeYouTubeComments={handleAnalyzeYouTubeComments} />
+                    </TabPanel>
+                </Tabs>
+
+                {/* Results always visible */}
+                <div className="results-container">
+                    <ResultsTable results={results} />
+
+                    {/* Sentiment Percentages displayed only if data exists */}
+                    {hasSentimentData && (
+                        <div className="sentiment-results">
+                            <h2>Sentiment Results:</h2>
+                            <ul>
+                                {Object.entries(sentimentPercentages).map(([key, value]) => (
+                                    <li key={key}>
+                                        {value.Negative && <span> Negative: {Number(value.Negative).toFixed(2)}% </span>}
+                                        {value.Positive && <span> Positive: {Number(value.Positive).toFixed(2)}% </span>}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                 </div>
             </>
         )}
     </div>
 );
-
 }
+
+
 
 export default App;
